b1f8f4cbf52a17927888ca63ae2551af10f91bc7
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2024-06-15 13:48:08 +0200

    Dont use native USB-CDC, add more example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
-void setup() {
+#define LED_PIN 33
 
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-    
+
+  digitalWrite(LED_PIN, HIGH);
+  Serial.println("LED HIGH");
+  delay(100);
+  digitalWrite(LED_PIN, LOW);
+  Serial.println("LED LOW");
+  delay(100);
 }
